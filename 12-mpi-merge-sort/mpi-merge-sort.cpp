8b2f14692059ca3cfd0571a9040e476c3e015824
mertcekuc <cekucmert@gmail.com>
2026-08-18 19:46:38 +0300

    mpi merge sort bug fixes and optimizations

--- 12-mpi-merge-sort/mpi-merge-sort.cpp
+++ 12-mpi-merge-sort/mpi-merge-sort.cpp
@@ -2,167 +2,163 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <array>
 
-
-#define NUM_ELEMENTS 64
+#define NUM_ELEMENTS 64 * 1024 * 2 * 2
 void fill_array(std::vector<int> &arr);
 void merge_sort(std::vector<int> &arr);
-std::vector<int> merge_arrs(std::vector<int> left, std::vector<int> right);
+std::vector<int> merge_arrs(const std::vector<int> &left, const std::vector<int> &right);
 
-
-
-void fill_array(std::vector<int> &arr){
+void fill_array(std::vector<int> &arr)
+{
     srand(time(0));
-    for(size_t i=0;i< arr.size(); i++){
-        arr[i]=rand()%NUM_ELEMENTS;
+    for (size_t i = 0; i < arr.size(); i++)
+    {
+        arr[i] = rand() % NUM_ELEMENTS;
     }
 }
 
-void merge_sort(std::vector<int> &arr){
+void merge_sort(std::vector<int> &arr)
+{
     size_t size{arr.size()};
-    if(size<=1){
+    if (size <= 1)
+    {
         return;
     }
-    else{
-        size_t mid{size/2};
-        std::vector<int>arr_l(arr.begin(),arr.begin()+mid);
-        std::vector<int>arr_r(arr.begin()+mid,arr.end());
 
-        size_t size_l{arr_l.size()};
-        size_t size_r{arr_r.size()};
-        if(size_l>1){
-            merge_sort(arr_l);
-        }
-        if(size_r>1){
-            merge_sort(arr_r);
-        }
+    size_t mid{size / 2};
+    std::vector<int> arr_l(arr.begin(), arr.begin() + mid);
+    std::vector<int> arr_r(arr.begin() + mid, arr.end());
 
-        if(size_l == 1 && size_r == 1){
-            arr = merge_arrs(arr_l,arr_r);
-            return;
-        }
-        
-    }
-    
+    size_t size_l{arr_l.size()};
+    size_t size_r{arr_r.size()};
+
+    merge_sort(arr_l);
+    merge_sort(arr_r);
+
+    arr = merge_arrs(arr_l, arr_r);
 }
-std::vector<int> merge_arrs(std::vector<int> left, std::vector<int> right){
+
+std::vector<int> merge_arrs(const std::vector<int> &left, const std::vector<int> &right)
+{
     size_t size_l{left.size()};
     size_t size_r{right.size()};
-
+    size_t l{0}, r{0};
     std::vector<int> result;
-    result.reserve(size_l+size_r);
+    result.reserve(size_l + size_r);
 
-    for(size_t i = 0; i< result.size();i++){
-        if(left.size()==0){
-            result[i]=right.front();
-            right.erase(right.begin());
+    for (size_t i = 0; i < size_l + size_r; i++)
+    {
+        if (l >= size_l)
+        {
+            result.push_back(right[r]);
+            r++;
             continue;
         }
-        else if(right.size()==0){
-            result[i]=left.front();
-            left.erase(left.begin());
+        else if (r >= size_r)
+        {
+            result.push_back(left[l]);
+            l++;
             continue;
         }
-        else{
-            if(left.front()<right.front()){
-                result[i]=left.front();
-                left.erase(left.begin());
+        else
+        {
+            if (left[l] < right[r])
+            {
+                result.push_back(left[l]);
+                l++;
                 continue;
             }
-            else{
-                result[i]=right.front();
-                right.erase(right.begin());
+            else
+            {
+                result.push_back(right[r]);
+                r++;
                 continue;
             }
         }
     }
 
     return result;
-
-
 }
 
-
-int main(){
-    MPI_Init(NULL,NULL);
+int main()
+{
+    MPI_Init(NULL, NULL);
     std::vector<int> arr(NUM_ELEMENTS);
-    int rank,size;
-    
+    int rank, size;
+    double start_time;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    int num_per_proccess {NUM_ELEMENTS/size};
-    std::vector<int> local_arr(num_per_proccess);
+    int num_per_proccess{NUM_ELEMENTS / size};
+    std::vector<int> sendcounts(size, num_per_proccess);
+    int remaining{NUM_ELEMENTS % size};
+    for (int i = 0; i < remaining; i++)
+    {
+        sendcounts[i]++;
+    }
+    std::vector<int> displs(size, 0);
+    for (int i = 1; i < size; i++)
+    {
+        displs[i] = displs[i - 1] + sendcounts[i - 1];
+    }
+    std::vector<int> local_arr(sendcounts[rank]);
 
-    if (rank == 0){
-        std::cout << "Filling array..." << std::endl;
+    if (rank == 0)
+    {
         fill_array(arr);
-        std::cout << "Array filled." << std::endl;
-
-        std::cout << "Unsorted array: ";
-        for(auto i: arr){
-            std::cout << i << " ";
-        }
-        std::cout << std::endl;
-    }
-    MPI_Scatter(arr.data(),num_per_proccess,MPI_INT,local_arr.data(),num_per_proccess,MPI_INT,0,MPI_COMM_WORLD);
-    for(int i=0; i<size; i++){
-        if(i==rank){
-            std::cout << "Rank: " << rank << " received array: ";
-            for(auto i: local_arr){
-                std::cout << i << " ";
-            }
-            std::cout << std::endl;
-        }
-        MPI_Barrier(MPI_COMM_WORLD);
+        start_time = MPI_Wtime();
     }
 
-    if(rank==0){
+    MPI_Scatterv(arr.data(),
+                 sendcounts.data(),
+                 displs.data(),
+                 MPI_INT,
+                 local_arr.data(),
+                 local_arr.size(),
+                 MPI_INT,
+                 0,
+                 MPI_COMM_WORLD);
+
+    if (rank == 0)
+    {
         std::cout << "Sorting..." << std::endl;
     }
-    MPI_Barrier(MPI_COMM_WORLD);
+
     merge_sort(local_arr);
-    for(int i=0; i<size; i++){
-        if(i==rank){
-            std::cout << "Rank: " << rank << " sorted array: ";
-            for(auto i: local_arr){
-                std::cout << i << " ";
-            }
-            std::cout << std::endl;
-            std::flush(std::cout);
-        }
-        MPI_Barrier(MPI_COMM_WORLD);
-    }
 
-    int step=1;
-    int current;
-    while(step<=size/2){
-        current=1;
-        while(current<size){
-            if(current==rank){
+    int step = 1;
+    
+    while (step <size)
+    {
+            if (rank%(step * 2) == step )
+            {
                 MPI_Ssend(local_arr.data(),
-                local_arr.size(),
-                MPI_INT,rank-step,
-                0,MPI_COMM_WORLD);
+                          local_arr.size(),
+                          MPI_INT, rank-step,
+                          0, MPI_COMM_WORLD);
             }
-            else{
-                std::vector<int> msg(local_arr.size());
-                MPI_Recv(msg.data(),local_arr.size(),MPI_INT,current,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-                local_arr = merge_arrs(local_arr,msg);
+            else if (rank%(step * 2) == 0)
+            {
+                std::vector<int> recv_arr(local_arr.size());
+                MPI_Recv(recv_arr.data(),
+                         recv_arr.size(),
+                         MPI_INT, rank+step,
+                         0, MPI_COMM_WORLD,
+                         MPI_STATUS_IGNORE);
+                local_arr = merge_arrs(local_arr, recv_arr);
             }
-            current += step*2;
-        }
-        MPI_Barrier(MPI_COMM_WORLD);
+        
         step *= 2;
-
-
+    }
+    if (rank == 0)
+    {
+        double end_time = MPI_Wtime();
+        std::cout << std::endl;
+        std::cout << "Time taken: " << end_time - start_time << " seconds" << std::endl;
     }
 
     MPI_Finalize();
-    std::cout << "Rank: " << rank << " sorted array: ";
-    for(auto i: local_arr){
-        std::cout << i << " ";
-    }
-    std::cout << std::endl;
+
     return 0;
 }
